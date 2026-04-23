638f7665fd943d7e406daa63b0febc23162f7f05
polimerakoushik <polimerakoushik0@gmail.com>
2026-04-23 16:56:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-async function harry(){
-    let data=await fetch("https://potterapi-fedeperin.vercel.app")
+async function api(){
+    let data=await fetch("https://corporatebs-generator.sameerkumar.website/")
     let fd=await data.json();
-    // let result=fd;
-    document.getElementById("block").innerText=fd
+    let result=fd.phrase
+    document.getElementById('res').innerHTML=result
 
 }
